9b130627ae555dfe5370ccc465ee7d34d3f9f180
Souparno Paul <souparnopaul@gmail.com>
2020-12-14 22:36:38 +0530

    Styling of on screen elements

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let canvas = document.getElementById("gameArea");
-canvas.width = window.innerHeight * 0.9;
-canvas.height = window.innerHeight * 0.9;
+let canvas = document.getElementById("canvas");
+canvas.width = document.documentElement.clientHeight * 0.8;
+canvas.height = document.documentElement.clientHeight * 0.8;
 let ctx = canvas.getContext("2d");
 
-let img = new Image();
-img.src = "board.png";
+let redPieceImg = document.getElementById("red-piece");
+let bluePieceImg = document.getElementById("blue-piece");
 
-img.addEventListener("load", () => {
-  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-});
+const side = canvas.width / 10;
+const offset = 37.5;
+let i = 0;
+let interval;
 
 const ladders = [
   [2, 23],
@@ -31,4 +32,34 @@ const snakes = [
   [43, 17],
 ];
 
-const side = canvas.width / 10;
+var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+
+[].forEach.call( imgs, function( img ) {
+    if(img.complete)
+      incrementCounter();
+    else
+      img.addEventListener( 'load', incrementCounter, false );
+} );
+
+function incrementCounter() {
+    counter++;
+    if ( counter === len ) {
+        console.log( 'All images loaded!');
+        interval = setInterval(game,1000);
+        // ctx.drawImage(bluePieceImg,40,canvas.height-50,30,40);
+    }
+}
+
+function game(){
+
+  if(i == 9){
+    clearInterval(interval);
+  }
+
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.drawImage(redPieceImg,offset + i*side - 15,canvas.height-50,30,40);
+  i++;
+
+}
