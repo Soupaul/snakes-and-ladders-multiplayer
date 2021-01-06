cfa2bf8500c217cab617761a7adc91cc66c4ed46
Nirvik <nirvik.agarwal@gmail.com>
2021-01-07 03:23:14 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
